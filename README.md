576eb43e97c970fec4625485985c4ab95a1e7c2f
Omar Letona <omarletonaf@gmail.com>
2021-02-16 14:28:59 -0600

    Added password validation

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
+# Header H1 
+**
 Practica 2 - Arqui 1
 Esta practica esta construida con un Arduino Mega
 Con la siguiente distruccion de pines:
+**
 
+## Sub Topic Headings H2
 Keypad - Arduino
-Pin A - Pin 23
-Pin B - Pin 25
-Pin C - Pin 27
-Pin D - Pin 29
-Pin 1 - Pin 49
-Pin 2 - Pin 51
-Pin 3 - Pin 53
+- Pin A - Pin 23
+- Pin B - Pin 25
+- Pin C - Pin 27
+- Pin D - Pin 29
+- Pin 1 - Pin 49
+- Pin 2 - Pin 51
+- Pin 3 - Pin 53
--- practica2.ino
+++ practica2.ino
@@ -2,9 +2,15 @@
 #include <Keypad.h>
 #include <Servo.h>
 
+//Definiciones
+char entry[6] = {'2','0','2','1','1','1'};
+int key_counter = 0;
+bool error = false;
+
 // Se definen las filas y columnas del Keypad
 const byte ROWS = 4;
 const byte COLUMNS = 3;
+
 //Definimos la estructura del keypad
 char keys[ROWS][COLUMNS] ={
   '1','2','3',
@@ -13,11 +19,46 @@ char keys[ROWS][COLUMNS] ={
   '*','0','#'
 };
 
-void setup() {
-  
+// Estructura de pines que van conectados al keypad
+byte rowPins[ROWS] = {23,25,27,29};
+byte columnPins[COLUMNS] = {49,51,53};
+//Teclado
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, ROWS, COLUMNS);
 
+void setup() {
+  Serial1.begin(9600);
+  pinMode(4, OUTPUT);
+  pinMode(9,OUTPUT);
 }
 
 void loop() {
+  if(key_counter<6){
+    char key = keypad.getKey();
+    if(key != NO_KEY){
+      if(entry[key_counter]!=key){error=true;}
+      Serial1.println(key);  
+      key_counter++;
+    } 
+  }else{
+    if(error){
+      digitalWrite(9, HIGH);  
+      key_counter = 0;
+      error=false;
+    }else{
+      digitalWrite(4, HIGH);
+    }
+    delay(2000);
+    digitalWrite(9, LOW);
+    digitalWrite(4, LOW);
+  }
+  
+}
 
+int getFreeIndex(){
+  for(int i = 0; i<5; i++){
+    if(entry[i]== 'x'){
+      return i;  
+    }
+  }  
+  return 'n';
 }
