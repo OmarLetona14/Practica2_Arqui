6bce479f859ae2753876b47cd6eab19ce7a8cf1a
Omar Letona <omarletonaf@gmail.com>
2021-02-16 13:23:37 -0600

    Updated README, added keypad structure

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 Practica 2 - Arqui 1
 Esta practica esta construida con un Arduino Mega
 Con la siguiente distruccion de pines:
+
+Keypad - Arduino
+Pin A - Pin 23
+Pin B - Pin 25
+Pin C - Pin 27
+Pin D - Pin 29
+Pin 1 - Pin 49
+Pin 2 - Pin 51
+Pin 3 - Pin 53
--- practica2.ino
+++ practica2.ino
@@ -2,12 +2,22 @@
 #include <Keypad.h>
 #include <Servo.h>
 
+// Se definen las filas y columnas del Keypad
+const byte ROWS = 4;
+const byte COLUMNS = 3;
+//Definimos la estructura del keypad
+char keys[ROWS][COLUMNS] ={
+  '1','2','3',
+  '4','5','6',
+  '7','8','9',
+  '*','0','#'
+};
+
 void setup() {
   
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
